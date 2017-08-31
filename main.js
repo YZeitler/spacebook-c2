d1e1ba18b08c49c0442e89aced8c3e2a3bbfc5cd
YZeitler <amz61@msn.com>
2017-08-31 11:52:55 +0300

    Post exercise 3 - Added a post, and removed said post

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
 posts = [];
 var id = 0;
 
+
 $('#post-button').click(function() {
-            function addPost(post, id) {
-                post = {
-                    text: post,
-                    id: id
-                }
-                posts.push(post);
-                id += 1;
+    id += 1;
 
-                function postToDiv() {
-                    for (i = 0; i < posts.length; i++) {
-                        $('.posts').append("" + posts[i].text + ", " + posts[i].id + "");
-                    }
-                }
+    function addPost(postVal, id) {
+        var postVal = $("#post-name").val();
+        post = {
+            text: postVal,
+            id: id
+        }
+        posts.push(post);
 
-            }
+    }
+
+
+    function postToDiv() {
+        $('.posts').empty();
+
+        for (i = 0; i < posts.length; i++) {
+            $('.posts').append('<p class="post"' + 'data-id="' + posts[i].id + '"' + '>' + '<button type="button" class="remove"> ' + '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span> REMOVE </button>' + posts[i].text + '  ' + '</p>');
+        }
+        removePost();
+    }
+    addPost('post', id);
+    postToDiv();
+
+
+    function removePost() {
+        $('.remove').on('click', function() {
+            var pID = $(this).closest('p').text;
+
+            //step 1: remove the 'relevant' post from the array
+            posts.splice(pID, 1);
+
+            //step 2: redisplay the current array status.
+            postToDiv();
+
+
+
+
+        });
+    }
+});
