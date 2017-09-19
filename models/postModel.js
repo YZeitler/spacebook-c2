3f6c481c85048b6608e734d04ea21504f06ec8f3
YZeitler <amz61@msn.com>
2017-09-19 18:10:13 +0300

    added schemata

--- models/postModel.js
+++ models/postModel.js
@@ -1,18 +1,23 @@
 var mongoose = require('mongoose');
-
+var Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost/spacebook');
 //design the two schema below and use sub docs 
 //to define the relationship between posts and comments
 
 //you don't need a comments collection
 //you only need a posts collection
 
-var commentSchema = new mongoose.Schema({
-
+var commentSchema = new Schema({
+    text: String,
+    user: String
 });
 
+var Comment = mongoose.model("comment", commentSchema);
 
-var postSchema = new mongoose.Schema({
 
+var postSchema = new Schema({
+    text: String,
+    comments: [commentSchema]
 });
 
 var Post = mongoose.model('post', postSchema)
